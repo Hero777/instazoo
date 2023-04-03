419a84b4c70b9ab8385916ae016d7d4120a65694
Hero777 <clickker@mail.ru>
2023-04-03 17:56:24 +0200

    4 003 CustomUserDetailsService класс

--- src/main/java/com/example/instazoo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/instazoo/service/CustomUserDetailsService.java
@@ -1,11 +1,17 @@
 package com.example.instazoo.service;
 
+import com.example.instazoo.entity.User;
 import com.example.instazoo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -19,7 +25,25 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username: " + username));
 
-        return null;
+        return build(user);
+    }
+
+    public User loadUserById(Long id) {
+        return userRepository.findUserById(id).orElse(null);
+    }
+
+    public static User build(User user) {
+        List<GrantedAuthority> authorities = user.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+        return new User(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
 }
